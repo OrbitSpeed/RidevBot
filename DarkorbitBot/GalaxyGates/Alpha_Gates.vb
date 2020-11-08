d658608dd3cd1410895e905ff7fed0e2635046e8
RisedSky <RisedSky@gmail.com>
2020-11-08 02:38:30 +0100

    Update avec encrypteur

--- DarkorbitBot/GalaxyGates/Alpha_Gates.vb
+++ DarkorbitBot/GalaxyGates/Alpha_Gates.vb
@@ -25,8 +25,11 @@ Public Class Alpha_Gates
 
     Public Shared pointer_scan As Integer = "400"
 
-    Public Shared Function Search_current_waves()
+    Public Shared Async Function Search_current_waves() As Task
 
+Return_start:
+
+        Form_Tools.WebClient_POST.Headers.Clear()
         Form_Tools.WebClient_POST.Headers.Add(HttpRequestHeader.Cookie, $"dosid={Utils.dosid};") 'POST / GET socket Information
         Dim WebClient_Data = Form_Tools.WebClient_POST.DownloadString("https://" + Utils.server + ".darkorbit.com/flashinput/galaxyGates.php?userID=" + Utils.userid + "&action=init&sid=" + Utils.dosid)
         Dim WebClient_GET_All_elements = Regex.Match(WebClient_Data, "<gate (.*id=""1" + Table_Load).Groups.Item(1).ToString ' ALPHA
@@ -34,52 +37,83 @@ Public Class Alpha_Gates
 
         Dim WebClient_GET_All_elements_currentWave = Regex.Match(WebClient_GET_All_elements, "currentWave="".*?([\s\S]*?)""").Groups.Item(1).ToString
 
-        If WebClient_GET_All_elements_currentWave = "0" Or "1" Or "2" Or "3" Or "4" Then
+        If WebClient_GET_All_elements_currentWave = "0" Or
+           WebClient_GET_All_elements_currentWave = "1" Or
+           WebClient_GET_All_elements_currentWave = "2" Or
+           WebClient_GET_All_elements_currentWave = "3" Or
+           WebClient_GET_All_elements_currentWave = "4" Then
             Current_npc = "streuner"
             Distance_npc = streuner_alpha
             control_killed_selection = "3"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "5" Or "6" Or "7" Or "8" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "5" Or
+            WebClient_GET_All_elements_currentWave = "6" Or
+            WebClient_GET_All_elements_currentWave = "7" Or
+            WebClient_GET_All_elements_currentWave = "8" Then
             Current_npc = "lordakia" ' df
             Distance_npc = Lordakia_alpha
             control_killed_selection = "5"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "9" Or "10" Or "11" Or "12" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "9" Or
+            WebClient_GET_All_elements_currentWave = "10" Or
+            WebClient_GET_All_elements_currentWave = "11" Or
+            WebClient_GET_All_elements_currentWave = "12" Then
             Current_npc = "mordon"
             Distance_npc = mordon_alpha
             control_killed_selection = "8"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "13" Or "14" Or "15" Or "16" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "13" Or
+            WebClient_GET_All_elements_currentWave = "14" Or
+            WebClient_GET_All_elements_currentWave = "15" Or
+            WebClient_GET_All_elements_currentWave = "16" Then
             Current_npc = "saimon" ' df
             Distance_npc = saimon_alpha
             control_killed_selection = "10"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "17" Or "18" Or "19" Or "20" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "17" Or
+            WebClient_GET_All_elements_currentWave = "18" Or
+            WebClient_GET_All_elements_currentWave = "19" Or
+            WebClient_GET_All_elements_currentWave = "20" Then
             Current_npc = "devolarium"
             Distance_npc = devolarium_alpha
             control_killed_selection = "15"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "21" Or "22" Or "23" Or "24" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "21" Or
+            WebClient_GET_All_elements_currentWave = "22" Or
+            WebClient_GET_All_elements_currentWave = "23" Or
+            WebClient_GET_All_elements_currentWave = "24" Then
             Current_npc = "kristallin"
             Distance_npc = kristallin_alpha
             control_killed_selection = "9"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "25" Or "26" Or "27" Or "28" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "25" Or
+            WebClient_GET_All_elements_currentWave = "26" Or
+            WebClient_GET_All_elements_currentWave = "27" Or
+            WebClient_GET_All_elements_currentWave = "28" Then
             Current_npc = "sibelon"
             Distance_npc = sibelon_alpha
             control_killed_selection = "15"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "29" Or "30" Or "31" Or "32" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "29" Or
+            WebClient_GET_All_elements_currentWave = "30" Or
+            WebClient_GET_All_elements_currentWave = "31" Or
+            WebClient_GET_All_elements_currentWave = "32" Then
             Current_npc = "sibelonit"
             Distance_npc = sibelonit_alpha
             control_killed_selection = "9"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "33" Or "34" Or "35" Or "36" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "33" Or
+            WebClient_GET_All_elements_currentWave = "34" Or
+            WebClient_GET_All_elements_currentWave = "35" Or
+            WebClient_GET_All_elements_currentWave = "36" Then
             Current_npc = "kristallon"
             Distance_npc = kristallon_alpha
             control_killed_selection = "9"
 
-        ElseIf WebClient_GET_All_elements_currentWave = "37" Or "38" Or "39" Or "40" Then
+        ElseIf WebClient_GET_All_elements_currentWave = "37" Or
+            WebClient_GET_All_elements_currentWave = "38" Or
+            WebClient_GET_All_elements_currentWave = "39" Or
+            WebClient_GET_All_elements_currentWave = "40" Then
             Current_npc = "protegit" 'df
             Distance_npc = protegit_alpha
             control_killed_selection = "15"
@@ -89,15 +123,9 @@ Public Class Alpha_Gates
         Console.WriteLine(Current_npc)
         Console.WriteLine(Distance_npc)
         Console.WriteLine(control_killed_selection)
-        Gates_task_task()
-
-    End Function
-
-
-    Public Shared Async Function Gates_task_task() As Task
+        'Gates_task_task()
 
 Base_macro:
-
         Dim Npc_in_map = Var.AutoIt.PixelSearch(595, 465, 785, 595, 13369344, 0, 1) ' detecter dans une plus petite zone pour savoir si il reste des npc avant d'attaquer les coins 
 
         If Npc_in_map.ToString.Contains(Var.Student) Then
@@ -138,7 +166,6 @@ Return_in_traveling:
 
             Else
                 Var.Update_Screen()
-                Dim traveling_indication_checking_true As Bitmap = My.Resources.traveling_indication
                 Dim traveling_indication_checking As Point = Var.Client_Screen.Contains(traveling_indication)
                 If traveling_indication_checking <> Nothing Then
                     Console.WriteLine("traveling indication trouvée")
@@ -189,13 +216,15 @@ Return_OnLock:
                     If InLocked(0) >= pointer_scan Then
                         If InLocked(0) - Distance_npc <= pointer0 Then
                             Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, 1, InLocked(1))
-                        Else Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, InLocked(0) - Distance_npc, InLocked(1))
+                        Else
+                            Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, InLocked(0) - Distance_npc, InLocked(1))
                         End If
                     Else
                         If InLocked(0) + Distance_npc >= pointer800 Then
                             Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, 799, InLocked(1))
-                        Else Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, InLocked(0) + Distance_npc, InLocked(1))
-                        End If ' ajouter un changer de direction tout les 200x exemple
+                        Else
+                            Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, InLocked(0) + Distance_npc, InLocked(1))
+                        End If ' ajouter un changeur de direction tous les 200x exemple
 
                     End If
 
@@ -212,7 +241,7 @@ Return_OnLock:
                     Else
                         Control_killed_searching = Control_killed_searching + 1
                         Console.WriteLine("Verification du lock")
-                        Await Task.Delay(10)
+                        Await Task.Delay(50)
                         GoTo Return_OnLock
 
                     End If
@@ -238,8 +267,9 @@ Return_OnLock:
 
                 Console.WriteLine("Wave terminated")
 
-                If Form_Tools.CheckBox_kill_alpha_lordakia.Checked = True Then ' CHANGER >>>    CheckBox_kill_alpha_lordakia > si npc alors plus haut alors un autre plus bas etc ! l'endroit du portail change selon la vague !
-                    ' sert aussi a definir si on veut passer a la vague d'apres ou sarreter avec la checkbox kill >>>
+                If Form_Tools.CheckBox_kill_alpha_lordakia.Checked = True Then
+                    ' CHANGER >>>    CheckBox_kill_alpha_lordakia > si npc alors plus haut alors un autre plus bas etc ! l'endroit du portail change selon la vague !
+                    ' sert aussi a definir si on veut passer a la vague d'apres ou s arreter avec la checkbox kill >>>
 
                     Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, 683, 522) ' portail gauche
                     Var.AutoIt.ControlClick("RidevBot", "", "[CLASS:MacromediaFlashPlayerActiveX; INSTANCE:1]", "left", 1, 400, 300)
@@ -262,7 +292,6 @@ Return_traveling_map:
 
                 Else
                     Var.Update_Screen()
-                    Dim traveling_indication_checking_true As Bitmap = My.Resources.traveling_indication
                     Dim traveling_indication_checking As Point = Var.Client_Screen.Contains(traveling_indication)
                     If traveling_indication_checking <> Nothing Then
                         Console.WriteLine("traveling indication trouvée")
@@ -280,11 +309,14 @@ Return_traveling_map:
             Else
                 Console.WriteLine("Checking Waves in progress..")
                 Await Task.Delay(2000)
-                Search_current_waves()
-                Exit Function
+                GoTo Return_start
+                'permet de revenir au début
+
+                'Search_current_waves()
+                'Exit Function
             End If
 
-        End if
+        End If
 
     End Function
 
